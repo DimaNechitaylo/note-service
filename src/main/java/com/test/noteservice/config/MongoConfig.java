f9052143ca966f4d70bbfa796d87552c26d42298
Dima1648 <dima.nechitaylo81@gmail.com>
2023-04-27 16:16:29 +0300

    added configs for mongo and sprignWeb

---
+++ src/main/java/com/test/noteservice/config/MongoConfig.java
@@ -0,0 +1,27 @@
+package com.test.noteservice.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.test.noteservice.repository")
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "notepad";
+    }
+
+}
